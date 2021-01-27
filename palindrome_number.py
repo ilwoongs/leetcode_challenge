2ba31b3d3b0c72a95a89fa1f87e77ccd6cf954be
ilwoongs <ilwoonge@gmail.com>
2021-01-28 02:29:01 +0900

    Palindrome Number

---
+++ palindrome_number.py
@@ -0,0 +1,21 @@
+"""
+9. Palindrome Number
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+"""
+
+//This algorithm compares characters from start to end with characters from end to start to check whether the input is palindrome number
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        xStr = str(x)
+        i = 0
+        j = len(xStr)-1
+        
+        while i<=j:
+            if xStr[i] != xStr[j]:
+                return False
+            i += 1;
+            j -= 1;
+        return True
+        
